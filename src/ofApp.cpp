f1c9a17b5e9d9dfda99b4d32abc84760732cf1bc
Jools <jools@Joolss-MBP.eduroam.student>
2018-01-07 20:16:43 +0000

    sorted

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,15 +1,7 @@
-/* This is an example of how to integrate maximilain into openFrameworks,
- including using audio received for input and audio requested for output.
- 
- 
- You can copy and paste this and use it as a starting example.
- 
- */
+/* additive synthesis with filters and oscillators  */
 
 #include "ofApp.h"
 
-
-//--------------------------------------------------------------
 void ofApp::setup(){
     
     ofSetFrameRate(60);
@@ -23,37 +15,11 @@ void ofApp::setup(){
     // calling the calling the setup method in maximilian settings containing samplerate and buffer size
     ofxMaxiSettings::setup(sampleRate, 2, bufferSize);
     
-    /* Anything that you would normally find/put in maximilian's setup() method needs to go here. For example, Sample loading.
-     */
-    
-    // apply ADSR values accordingly
-    ADSR.attack = 200;
-    ADSR.decay = 200;
-    ADSR.sustain = 50;
-    ADSR.release = 3000;
-    
     mySample.load(ofToDataPath("sound.wav"));
     
     ofBackground(0,0,0);
 
-    ofSoundStreamSetup(2,2,this, sampleRate, bufferSize, 4); /* this has to happen at the end of setup - it switches on the DAC */
-
-}
-
-//--------------------------------------------------------------
-void ofApp::update(){
-    
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-    
-    /* You can use any of the data from audio received and audiorequested to draw stuff here.
-     
-     */
-    
-    
+    ofSoundStreamSetup(2,2,this, sampleRate, bufferSize, 4);
 }
 
 //--------------------------------------------------------------
@@ -65,16 +31,17 @@ void ofApp::audioOut(float * output, int bufferSize, int nChannels) {
         // LFO
         LFO1out = LFO1.sinewave(5) * 2;
         LFO2out = LFO2.sinewave(0.1) * 1500;
- 
-        // clock device using phasor and square wave
-        
+    
         int myArray[10] = {100, 200, 300, 400, 500, 600, 500, 400, 300, 200};
+        
+        // clock device using phasor
         currentCount = myCounter.phasor(1, 1, 9);
         
         //--- oscillators ---//
         
         // worth playing around with high pass / band pass / band reject
-        VCO1out = VCO1.square(2000) * currentCount;
+        // square wave
+        VCO1out = VCO1.square(2000 * currentCount);
         
         // LFO controlled oscillator --> vibrato
         VCO2out = VCO2.sawn(200 + LFO1out);
@@ -87,10 +54,10 @@ void ofApp::audioOut(float * output, int bufferSize, int nChannels) {
         VCF2out = VCF2.lores(VCO2out, 2000 + LFO2out, 20);
         
         // amplitude control on raw oscillator and filter
-        double VCO1amp = VCO1out * 0.0;
-        double VCF1amp = VCF1out * 0.0;
+        double VCO1amp = VCO1out * 0.05;
+        double VCF1amp = VCF1out * 0.05;
         double VCF2amp = VCF2out * 0.05;
-        double VCO2amp = VCO2out * 0.0;
+        double VCO2amp = VCO2out * 0.05;
         
         // mixer
         mix = VCO1amp + VCF1amp + VCF2amp + VCO2amp;
@@ -99,66 +66,4 @@ void ofApp::audioOut(float * output, int bufferSize, int nChannels) {
         output[i * nChannels + 1] = mix;
         
     }
-    
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::audioIn(float * input, int bufferSize, int nChannels){
-    
-    for(int i = 0; i < bufferSize; i++){
-        /* you can also grab the data out of the arrays*/
-        
-    }
-    
-}
-
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-    
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-    
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-    
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-    
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
-    
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -7,39 +7,14 @@ class ofApp : public ofBaseApp{
 
 	public:
 		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-    
-    // Audio output and input methods
-    
         void audioOut(float * output, int bufferSize, int nChannels);
-        void audioIn(float * input, int bufferSize, int nChannels);
-    
-    
-    // stick you maximilian declarations below
     
     // buffer size
     int bufferSize;
     
     // sample rate
     int sampleRate;
-    
 
-    
-    // maxiClock object doesn't seem to exist in the OF version of Maxim :[
-    // maxiClock myClock;
-    // instead we'll create a counter with phasor
-    
     // set up oscillators
     maxiOsc VCO1, VCO2, myCounter, LFO1, LFO2;
     
